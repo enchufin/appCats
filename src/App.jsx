4f93a22c918a00c0455934a39e1da003f1470b23
enchufin <lincografic@gmail.com>
2024-11-03 18:35:40 +0100

    nuevo estado para imageTag con el que añadir un alt en la imagen e implementación de estilos a modo de cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import './styles.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWords}?fontSize=24&fontColor=white&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/'
@@ -6,6 +7,7 @@ const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/'
 export default function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  const [imageTag, setImageTag] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -21,17 +23,19 @@ export default function App () {
           .then(res => res.json())
           .then(response => {
             const idImage = response._id
+            setImageTag(response.tags)
             setImageUrl(`${idImage}/says/${firstWords}?fontSize=30&fontColor=white`)
           })
       })
   }, [])
 
   return (
-    <>
+    <main>
       <h1>App de gatitos</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} />}
-
-    </>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} atl={`imagen random de gatitos con los tags: ${imageTag}`} />}
+      </section>
+    </main>
   )
 }
